6d7974e95aea3055320f06c53b58567a37164419
- <=>
2021-12-27 15:45:15 +0700

    done

---
+++ src/com/codegym/Main.java
@@ -0,0 +1,13 @@
+package com.codegym;
+
+public class Main {
+
+    public static void main(String[] args) {
+        Car car1 = new Car("mazda3","Skyactiv 3");
+        System.out.println(Car.numberOfCar);
+        System.out.println(Car.numberOfCar);
+        Car car2 = new Car("mazda2","Skyactiv 2");
+        System.out.println(Car.numberOfCar);
+        System.out.println(Car.numberOfCar);
+    }
+}
